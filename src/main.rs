3dcf4a9a8d270cfa6a7d8b7725820bc1d602de04
Jaxel Rojas <jaxelr@gmail.com>
2019-05-17 10:08:23 -0400

    Chapter 2: finished, added error handling logic

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
 
     loop {
         
@@ -18,8 +18,10 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess:  u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess:  u32 =  match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
         println!("You guessed: {}", guess);
 
@@ -28,8 +30,8 @@ fn main() {
                 println!("You Win!");
                 break
             }
-            Ordering::Greater => println!("Too small"),
-            Ordering::Less => println!("too big")
+            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("too small")
         }
     }
 }
