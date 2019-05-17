a6a33eea619d896c220563f6a80ba04de0ee76d4
Jaxel Rojas <jaxelr@gmail.com>
2019-05-17 14:34:54 -0400

    Chapter 3: Data Types primitives

---
+++ Data_Types/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    println!("Hello, world!");
+
+    //Primitive Data Types
+
+    //Scalar
+    let val1: u8 = 16;
+    let val1: i8 = -16;
+
+    //These size integers reach until size 128 on 
+    
+    //u16
+    //u32
+    //u64
+    //u128
+
+    //Floating-Point Types
+    let fl = 2.0;
+    let xfl: f32 = 3.0;
+
+    println!("This is float {} and this is float32 {}", fl , xfl);
+
+    //Arithmetic operators
+    //addition
+    let sum = 5 + 10;
+    // subtraction
+    let difference = 95.5 - 4.3;
+    // multiplication
+    let product = 4 * 30;
+    // division
+    let quotient = 56.7 / 32.2;
+    // remainder
+    let remainder = 43 % 5;
+
+    //Boolean types
+    let t = true;
+    let f: bool = false; // with explicit type annotation
+
+    //Character Type
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+
+}
