f6a64f0f37c3dfcc915d5a5e4207c8fbe0b61f1c
Jaxel Rojas <jaxelr@gmail.com>
2019-05-20 11:13:36 -0400

    Chapter 3: Data Types - compound data types

--- Data_Types/src/main.rs
+++ Data_Types/src/main.rs
@@ -40,4 +40,42 @@ fn main() {
     let _c = 'z';
     let _z = 'ℤ';
     let _heart_eyed_cat = '😻';
+
+    //Compound Types
+
+    //Tuples
+    let tup = (500, 6.4, 1);
+
+    //Destructuring
+    let (_x, y, _z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    //Accessing elements of the tuple
+
+    let xtup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let _five_hundred = xtup.0;
+
+    let _six_point_four = xtup.1;
+
+    let _one = xtup.2;
+
+    //Arrays
+
+    let _months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    //Alternate declarations
+    let _array_declaration: [i32; 5] = [1, 2, 3, 4, 5];
+
+    //Concise declarations
+    let _short_array_declaration = [3; 5]; // [3, 3, 3, 3, 3]
+    
+    let tiny_array = [1, 2, 3, 4, 5];
+    let index = 4;
+
+    let element = tiny_array[index];
+
+    println!("The value of element is: {}", element);
 }   
