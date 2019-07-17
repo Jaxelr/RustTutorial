7cd0e4610183633a5a93a7a821d1c1a871946d3b
Jaxel Rojas <jaxelr@gmail.com>
2019-07-17 17:22:52 -0400

    chapter: 7_2 modules

---
+++ 7_2_restaurant/src/lib.rs
@@ -0,0 +1,15 @@
+mod front_of_house {
+    mod hosting {
+        fn _add_to_waitlist() {}
+
+        fn _seat_at_table() {}
+    }
+
+    mod serving {
+        fn _take_order() {}
+
+        fn _serve_order() {}
+
+        fn _take_payment() {}
+    }
+}
