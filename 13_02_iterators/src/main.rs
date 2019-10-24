39a2c820428ccf0af5041216b1ed8795e760854c
Jaxel Rojas <jaxelr@gmail.com>
2019-10-24 16:06:46 -0400

    chapter: 13_02 Iterators

---
+++ 13_02_iterators/src/main.rs
@@ -0,0 +1,101 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter()
+         .filter(|s| s.size == shoe_size)
+         .collect()
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    let v2 = vec![1, 2, 3];
+
+    let mut v2_iter = v2.iter();
+
+    //If we use next, we have to make our code mutable
+    assert_eq!(v2_iter.next(), Some(&1));
+    assert_eq!(v2_iter.next(), Some(&2));
+    assert_eq!(v2_iter.next(), Some(&3));
+    assert_eq!(v2_iter.next(), None);
+
+    let v3 = vec![1, 2, 3];
+    let v3_iter = v3.iter();
+
+    //cant use v3_iter after this instruction
+    let total: i32 = v3_iter.sum();
+
+    assert_eq!(total, 6);
+
+    let v4: Vec<i32> = vec![1, 2, 3];
+    let v5: Vec<_> = v4.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v5, vec![2, 3, 4]);    
+
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker") },
+        Shoe { size: 13, style: String::from("sandal") },
+        Shoe { size: 10, style: String::from("boot") },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 10, style: String::from("boot") },
+        ]
+    );
+
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+
+    let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+                                 .map(|(a, b)| a * b)
+                                 .filter(|x| x % 3 == 0)
+                                 .sum();
+    assert_eq!(18, sum);
+}
+
