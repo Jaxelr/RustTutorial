84272281a8125c33cc78fefe1ab6525b41f3508f
Jaxel Rojas <jaxelr@gmail.com>
2019-11-04 11:04:16 -0400

    chapter: 14_04 installing binaries

---
+++ 14_04_cargo_install/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+//This is a placeholder to understand how to install
+//binaries on your machine using cargo install
+//just use cargo install <<package>>
+//ex: cargo install ripgrep
+
