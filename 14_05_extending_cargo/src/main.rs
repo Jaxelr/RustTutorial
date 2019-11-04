6b382f4c6240eb70f1dfe2713a63709201a35727
Jaxel Rojas <jaxelr@gmail.com>
2019-11-04 11:45:12 -0400

    chapter: 14_05 cargo extensions

---
+++ 14_05_extending_cargo/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/** 
+
+To install packages that extend cargo you must
+cargo install like you would with other commands
+
+ie: cargo install cargo-edit
+
+in order to see what is installed you can run the command
+
+cargo --list
+
+and to run it use the command 
+
+cargo <<package>> 
+
+*/
