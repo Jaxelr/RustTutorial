acb372a50911171e6675b2c010c3431b3edabfc0
Jaxel Rojas <jaxelr@gmail.com>
2019-11-20 16:52:31 -0400

    chapter: 16_04 extensible concurrency

---
+++ 16_04_transfer_ownership/src/main.rs
@@ -0,0 +1,8 @@
+use std::marker;
+
+fn main() {
+    //No code, just information about the traits sync and send that are implemented
+    //by the language.
+    //The way to implement concurrency is using the safe mechanism provided by the standard library
+    //such as: Mutex<T>, Arc<T>, RefCell<T>, Rc<T>, Arc<T>, etc... 
+}
