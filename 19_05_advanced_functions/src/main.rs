c82d9a6322d60cb172ab557effe85ecff3ba26c6
Jaxel Rojas <jaxelr@gmail.com>
2019-12-26 15:02:13 -0400

    chapter: 19_05 advanced functions

---
+++ 19_05_advanced_functions/src/main.rs
@@ -0,0 +1,37 @@
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+enum Status {
+    Value(u32),
+    _Stop,
+}
+
+//
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn main() {
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
+
+    //function as argument to map
+    let list_of_numbers = vec![1, 2, 3];
+    let _list_of_strings: Vec<String> = list_of_numbers
+        .iter()
+        .map(ToString::to_string)
+        .collect();
+
+    let _list_of_statuses: Vec<Status> =
+        (0u32..20)
+        .map(Status::Value)
+        .collect();
+
+    let _result = returns_closure();
+}
