8b58b3cd39aa1939e8598f87d56ce59590e20008
Jaxel Rojas <jaxelr@gmail.com>
2020-03-30 17:17:16 -0400

    chapter:  02_09 cargo home

---
+++ The Cargo Book/02_09_cargo_home/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Cargo home path contains:
+// config 
+// credentials
+// .crates.toml
+// registries
+// git
+
+// Useful cargo cli commands
+// Vendoring all dependencies of a project: cargo vendor
