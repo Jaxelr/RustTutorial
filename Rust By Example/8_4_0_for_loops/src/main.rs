a872d52102815cf47b779b3ce64b9409e0d268b3
Jaxel Rojas <jaxelr@gmail.com>
2020-06-23 08:16:33 -0400

    chapter: 8_4 for loops

---
+++ Rust By Example/8_4_0_for_loops/src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+    // `n` will take the values: 1, 2, ..., 100 in each iteration
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // `n` will take the values: 1, 2, ..., 100 in each iteration
+    // inclusive range a..=b
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    //iter borrows the collection and leaves the original unmodified
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+
+
+    }
+
+    //into_iter consumes the collection and disposes of it after
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    //into_mut borrows the collection with mutability
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        };
+
+        println!("Hello {}", name);
+    }
+}
