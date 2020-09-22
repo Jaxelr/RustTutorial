1efb8c941a4982449caa5a5f9a37d8f4d0c8252b
Jaxel Rojas <jaxelr@gmail.com>
2020-09-22 08:34:04 -0400

    chapter: 18_5_0 iterating over results

---
+++ Rust By Example/18_5_0_iterating_over_result/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+
+    //Iter::map usage
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .collect();
+    println!("Results map: {:?}", numbers);
+
+    //Filter map
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .filter_map(Result::ok)
+        .collect();
+    println!("Results with filter_map: {:?}", numbers);
+
+    // Return Result<Vec<T>, E>
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Result<Vec<_>, _> = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .collect();
+    println!("Results <Vec>: {:?}", numbers);
+
+    // Partition by Result Ok
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+    println!("Results Numbers: {:?}", numbers);
+    println!("Results Errors: {:?}", errors);
+
+    //Unwrap Result Vector
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+    let numbers: Vec<_> = numbers.into_iter().map(Result::unwrap).collect();
+    let errors: Vec<_> = errors.into_iter().map(Result::unwrap_err).collect();
+    println!("Iter Numbers: {:?}", numbers);
+    println!("Iter Errors: {:?}", errors);
+}
