85adeb42cecbfef11c97177292dfc4f07cfdea2b
Jaxel Rojas <jaxelr@gmail.com>
2021-01-12 14:58:19 -0400

    chapter: 2 2 concatenating strings

---
+++ Rust Design Patterns/02_02_0_concatenating_strings/src/main.rs
@@ -0,0 +1,14 @@
+fn say_hello(name: &str) -> String {
+    // We could construct the result string manually.
+    // let mut result = "Hello ".to_owned();
+    // result.push_str(name);
+    // result.push('!');
+    // result
+
+    // But using format! is better.
+    format!("Hello {}!", name)
+}
+
+fn main() {
+    println!("{}", say_hello("world"));
+}
