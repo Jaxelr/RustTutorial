5d56ca1e9c4805386a2b842fda6a4db96311b189
Jaxel Rojas <jaxelr@gmail.com>
2021-01-19 08:28:43 -0400

    chapter: 2 4 default trait

---
+++ Rust Design Patterns/02_04_0_default_trait/src/main.rs
@@ -0,0 +1,29 @@
+use std::{path::PathBuf, time::Duration};
+
+// note that we can simply auto-derive Default here.
+#[derive(Default, Debug)]
+struct MyConfiguration {
+    // Option defaults to None
+    output: Option<PathBuf>,
+    // Vecs default to empty vector
+    search_path: Vec<PathBuf>,
+    // Duration defaults to zero time
+    timeout: Duration,
+    // bool defaults to false
+    check: bool,
+}
+
+impl MyConfiguration {
+    // add setters here
+}
+
+fn main() {
+    // construct a new instance with default values
+    let mut conf = MyConfiguration::default();
+    // do something with conf here
+
+    conf.timeout = Duration::new(10, 10);
+
+    conf.check = true;
+    println!("conf = {:#?}", conf);
+}
