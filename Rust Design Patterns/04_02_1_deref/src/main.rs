c3678a2dae227e63c0c63e2a29afdfa778ddcdbe
Jaxel Rojas <jaxelr@gmail.com>
2021-03-08 13:02:08 -0400

    chapter: 04 02 1 deref polymorphism

---
+++ Rust Design Patterns/04_02_1_deref/src/main.rs
@@ -0,0 +1,25 @@
+use std::ops::Deref;
+
+struct Foo {}
+
+impl Foo {
+    fn m(&self) {
+    }
+
+}
+
+struct Bar {
+    f: Foo
+}
+
+impl Deref for Bar {
+    type Target = Foo;
+    fn deref(&self) -> &Foo {
+        &self.f
+    }
+}
+
+fn main() {
+    let b = Bar { f : Foo {} };
+    b.m();
+}
