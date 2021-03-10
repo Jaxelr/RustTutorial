dc291c2b589d997555b3ec1ff60322d6d74f19cf
Jaxel Rojas <jaxelr@gmail.com>
2021-03-10 12:19:51 -0400

    chapter: 01 01 1 random number generator

---
+++ Rust Cookbook/01_01_1_random_values/src/main.rs
@@ -0,0 +1,98 @@
+use rand::distributions::{Alphanumeric, Distribution, Standard, Uniform};
+use rand::{thread_rng, Rng};
+
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Distribution<Point> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point {
+        let (rand_x, rand_y) = rng.gen();
+        Point {
+            x: rand_x,
+            y: rand_y,
+        }
+    }
+}
+
+pub mod distributed {
+
+    use rand::thread_rng;
+    use rand_distr::{Distribution, Normal, NormalError};
+
+    pub fn random_dstr() -> Result<(), NormalError> {
+        let mut rng = thread_rng();
+        let normal = Normal::new(2.0, 3.0)?;
+        let v = normal.sample(&mut rng);
+        println!("{} is from a N(2, 9) distribution", v);
+        Ok(())
+    }
+}
+
+fn main() {
+    let mut rng = rand::thread_rng();
+
+    //Random
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen();
+    println!("Random u8: {}", n1);
+    println!("Random u16: {}", n2);
+    println!("Random u32: {}", rng.gen::<u32>());
+    println!("Random i32: {}", rng.gen::<i32>());
+    println!("Random float: {}", rng.gen::<f64>());
+
+    //Random within a range
+    let mut rng = rand::thread_rng();
+    println!("Integer: {}", rng.gen_range(0..10));
+    println!("Float: {}", rng.gen_range(0.0..10.0));
+
+    //Random with uniform distribution
+    let mut rng = rand::thread_rng();
+    let die = Uniform::from(1..7);
+
+    loop {
+        let throw = die.sample(&mut rng);
+        println!("Roll the die: {}", throw);
+        if throw == 6 {
+            println!("Got 6, getting out");
+            break;
+        }
+    }
+
+    //Random with custom type
+    let mut rng = rand::thread_rng();
+    let rand_tuple = rng.gen::<(i32, bool, f64)>();
+    let rand_point: Point = rng.gen();
+    println!("Random tuple: {:?}", rand_tuple);
+    println!("Random Point: {:?}", rand_point);
+
+    //Alphanumeric characters
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+
+    println!("{}", rand_string);
+
+    //Generate random passwords from a set of characters
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    abcdefghijklmnopqrstuvwxyz\
+    0123456789)(*&^%$#@!~";
+    const PASSWORD_LEN: usize = 30;
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+    println!("{:?}", password);
+
+    //Random numbers with given distribution
+    distributed::random_dstr().unwrap();
+}
