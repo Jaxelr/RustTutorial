2eeab3301753b113183abc302068d92c2e8a7b32
Jaxel Rojas <jaxelr@gmail.com>
2021-03-15 16:43:31 -0400

    chapter: 03 01 manage tarballs

---
+++ Rust Cookbook/03_01_tarballs/src/main.rs
@@ -0,0 +1,42 @@
+use std::fs::File;
+use std::path::PathBuf;
+use flate2::read::GzDecoder;
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use tar::Archive;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    //Decompress
+    let path = "archive.tar.gz";
+
+    let tar_gz = File::open(path)?;
+    let tar = GzDecoder::new(tar_gz);
+    let mut archive = Archive::new(tar);
+    archive.unpack(".")?;
+
+    //Compress
+    let tar_gz = File::create("archive.tar.gz")?;
+    let enc = GzEncoder::new(tar_gz, Compression::default());
+    let mut tar = tar::Builder::new(enc);
+    tar.append_dir_all("backup/logs", "/var/log")?; //We are appending the directories
+
+
+    let file = File::open("archive.tar.gz")?;
+    let mut archive = Archive::new(GzDecoder::new(file));
+    let prefix = "bundle/logs";
+
+    println!("Extracted the following files:");
+    archive
+        .entries()?
+        .filter_map(|e| e.ok())
+        .map(|mut entry| -> Result<PathBuf, Box<dyn std::error::Error>> {
+            let path = entry.path()?.strip_prefix(prefix)?.to_owned();
+            entry.unpack(&path)?;
+            Ok(path)
+        })
+        .filter_map(|e| e.ok())
+        .for_each(|x| println!("> {}", x.display()));
+
+    Ok(())
+}
