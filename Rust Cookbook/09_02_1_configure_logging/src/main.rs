f3dad0d335aad8a86b87ab7288f8bc5066d59167
Jaxel Rojas <jaxelr@gmail.com>
2021-03-31 08:25:17 -0400

    chapter: 09 02 configure logging

---
+++ Rust Cookbook/09_02_1_configure_logging/src/main.rs
@@ -0,0 +1,23 @@
+use std::io::Write;
+use chrono::Local;
+use env_logger::Builder;
+use log::LevelFilter;
+
+//Add local datetime
+fn main() {
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(buf,
+                "{} [{}] - {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+
+    log::warn!("warn");
+    log::info!("info");
+    log::debug!("debug");
+}
