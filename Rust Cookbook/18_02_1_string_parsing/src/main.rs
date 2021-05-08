f41215efcc0eaf81815788b064bcaa80c757386f
Jaxel Rojas <jaxelr@gmail.com>
2021-05-08 08:48:02 -0400

    chapter: 18 02  string parsings

---
+++ Rust Cookbook/18_02_1_string_parsing/src/main.rs
@@ -0,0 +1,63 @@
+use unicode_segmentation::UnicodeSegmentation;
+use std::str::FromStr;
+
+fn main() {
+    collect_unicode();
+    convert_str_to_rgb();
+}
+
+fn collect_unicode() -> () {
+    let name = "José Guimarães\r\n";
+    let graphemes = UnicodeSegmentation::graphemes(name, true)
+    	.collect::<Vec<&str>>();
+    assert_eq!(graphemes[3], "é");
+}
+
+#[derive(Debug, PartialEq)]
+struct RGB {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl FromStr for RGB {
+    type Err = std::num::ParseIntError;
+
+    // Parses a color hex code of the form '#rRgGbB..' into an
+    // instance of 'RGB'
+    fn from_str(hex_code: &str) -> Result<Self, Self::Err> {
+    
+        // u8::from_str_radix(src: &str, radix: u32) converts a string
+        // slice in a given base to u8
+        let r: u8 = u8::from_str_radix(&hex_code[1..3], 16)?;
+        let g: u8 = u8::from_str_radix(&hex_code[3..5], 16)?;
+        let b: u8 = u8::from_str_radix(&hex_code[5..7], 16)?;
+
+        Ok(RGB { r, g, b })
+    }
+}
+
+fn convert_str_to_rgb() -> () {
+    let code: &str = &r"#fa7268";
+    match RGB::from_str(code) {
+        Ok(rgb) => {
+            println!(
+                r"The RGB color code is: R: {} G: {} B: {}",
+                rgb.r, rgb.g, rgb.b
+            );
+        }
+        Err(_) => {
+            println!("{} is not a valid color hex code!", code);
+        }
+    }
+
+    // test whether from_str performs as expected
+    assert_eq!(
+        RGB::from_str(&r"#fa7268").unwrap(),
+        RGB {
+            r: 250,
+            g: 114,
+            b: 104
+        }
+    );
+}
