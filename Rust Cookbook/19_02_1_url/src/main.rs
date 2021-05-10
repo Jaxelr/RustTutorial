673cfe32d70c13c92bff09d9de834adcea473014
Jaxel Rojas <jaxelr@gmail.com>
2021-05-10 16:06:12 -0400

    chapter: 19 02 Url

---
+++ Rust Cookbook/19_02_1_url/src/main.rs
@@ -0,0 +1,45 @@
+use url::{Url, Position, Origin, Host, ParseError};
+
+fn main() -> Result<(), Box <dyn std::error::Error>> {
+    parse_url_from_string()?;
+    create_new_url()?;
+    remove_fragments()?;
+
+    Ok(())
+}
+
+fn parse_url_from_string() -> Result<(), ParseError> {
+    let s = "https://github.com/rust-lang/rust/issues?labels=E-easy&state=open";
+
+    let parsed = Url::parse(s)?;
+    println!("The path part of the URL is: {}", parsed.path());
+
+    Ok(())
+}
+
+fn create_new_url() -> Result<(), ParseError> {
+    let path = "/rust-lang/cargo";
+
+    let gh = build_github_url(path)?;
+
+    assert_eq!(gh.as_str(), "https://github.com/rust-lang/cargo");
+    println!("The joined URL is: {}", gh);
+
+    Ok(())
+}
+
+fn build_github_url(path: &str) -> Result<Url, ParseError> {
+    const GITHUB: &'static str = "https://github.com";
+
+    let base = Url::parse(GITHUB).expect("hardcoded URL is known to be valid");
+    let joined = base.join(path)?;
+
+    Ok(joined)
+}
+
+fn remove_fragments() -> Result<(), ParseError> {
+    let parsed = Url::parse("https://github.com/rust-lang/rust/issues?labels=E-easy&state=open")?;
+    let cleaned: &str = &parsed[..Position::AfterPath];
+    println!("cleaned: {}", cleaned);
+    Ok(())
+}
